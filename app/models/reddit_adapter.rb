446d18a0ab75380f07a43b96ea0d923663fe7e38
John Gregory <john@bellwethermade.com>
2018-01-02 01:15:29 -0800

    Parses comment data from reddit

--- app/models/reddit_adapter.rb
+++ app/models/reddit_adapter.rb
@@ -1,6 +1,20 @@
-class ReditAdapter
+class RedditAdapter
 
-  def fetch
+  def self.get_reddit_data
+
+    regex_union = Regexp.union([Coin.pluck(:sym), Coin.pluck(:name)].flatten)
+
+    # https://www.reddit.com/r/pics/search.json?sort=new
+    res = HTTP.get('https://www.reddit.com/r/cryptocurrency/comments.json')
+    json = JSON.parse(res.to_s)
+    comments = json['data']['children'].map{ |x| x['data']['body'] }
+    comments.each do |comment|
+      match = comment.match(regex_union)
+      if match
+        coin = Coin.find_by_sym(match.to_s)
+        coin = Coin.find_by_name(match.to_s) unless coin
+        Mention.create(coin_id: coin.id) if coin
+      end
+    end
   end
-
 end
