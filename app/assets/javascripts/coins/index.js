17e17bbf348ca22da50e16a15873cb72b404e6ad
John Gregory <john@bellwethermade.com>
2018-01-02 21:12:23 -0800

    fixes percentage colors

--- app/assets/javascripts/coins/index.js
+++ app/assets/javascripts/coins/index.js
@@ -5,27 +5,25 @@ Coin.init = function() {
     method: 'GET',
     url: 'https://api.coinmarketcap.com/v1/ticker/',
     success: function (data) {
-      console.log(data);
-      console.log('good');
       $.each(data, function (key, value) {
 
         $('.hour-' + value.symbol).html(value.percent_change_1h)
         $('.day-' + value.symbol).html(value.percent_change_7d)
         $('.week-' + value.symbol).html(value.percent_change_24h)
 
-        if (parseInt(value.percent_change_1h) > 1) {
+        if (parseFloat(value.percent_change_1h) < 0) {
           $('.hour-' + value.symbol).addClass('red')
         } else {
           $('.hour-' + value.symbol).addClass('green')
         }
 
-        if (parseInt(value.percent_change_7d) > 1) {
+        if (parseFloat(value.percent_change_7d) < 0) {
           $('.day-' + value.symbol).addClass('red')
         } else {
           $('.day-' + value.symbol).addClass('green')
         }
 
-        if (parseInt(value.percent_change_24h) > 1) {
+        if (parseFloat(value.percent_change_24h) < 0) {
           $('.week-' + value.symbol).addClass('red')
         } else {
           $('.week-' + value.symbol).addClass('green')
@@ -36,7 +34,7 @@ Coin.init = function() {
     },
     error: function(jxHR) {
       console.log(jxHR);
-      console.log('ya fucked up');
+      alert.log('coinbase api link hella broke');
     }
   })
 }
