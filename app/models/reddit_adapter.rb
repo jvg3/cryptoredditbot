896de9639da34564d5e5679189edc5e2a71f989e
John Gregory <john@bellwethermade.com>
2018-01-04 21:56:09 -0800

    updates lookup function to only grep for whole word

--- app/models/reddit_adapter.rb
+++ app/models/reddit_adapter.rb
@@ -1,19 +1,22 @@
 class RedditAdapter
 
   def self.get_reddit_data
-
-    regex_union = Regexp.union([Coin.pluck(:sym), Coin.pluck(:name)].flatten.map(&:upcase))
+    coin_regexes = Coin.all.map{ |coin| [" #{coin.sym}", "#{coin.sym} ", " #{coin.name}", "#{coin.name} "] }
 
     # https://www.reddit.com/r/pics/search.json?sort=new
     res = HTTP.get('https://www.reddit.com/r/cryptocurrency/comments.json')
     json = JSON.parse(res.to_s)
     comments = json['data']['children'].map{ |x| x['data']['body'] }
     comments.each do |comment|
-      match = comment.upcase.match(regex_union)
-      if match
-        coin = Coin.find_by_sym(match.to_s)
-        coin = Coin.find_by_name(match.to_s) unless coin
-        Mention.create(coin_id: coin.id, comment: comment) if coin
+
+      comment_up = comment.upcase
+      coin_regexes.each do |coin_regex|
+        match = comment_up.match(Regexp.union(coin_regex))
+        if match
+          coin = Coin.find_by_sym(match.to_s.strip)
+          coin = Coin.find_by_name(match.to_s.strip) unless coin
+          Mention.create(coin_id: coin.id, comment: comment) if coin
+        end
       end
     end
   end
