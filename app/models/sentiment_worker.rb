f25fdb3fefee38807b9f2b0b2014df0567bf723b
John Gregory <john@bellwethermade.com>
2018-01-08 01:15:27 -0800

    adds sentiment 2 hour

--- app/models/sentiment_worker.rb
+++ app/models/sentiment_worker.rb
@@ -5,7 +5,7 @@ class SentimentWorker
   # Sidekiq::Cron::Job.create(name: 'reddit_worker', cron: '7 * * * *', class: 'RedditWorker')
   def perform(*args)
     puts "STARTING SENTIMENT ANALYSIS"
-    Mention.where('sentiment IS NULL and created_at > ?', Time.now.utc - 1.hour).each do |mention|
+    Mention.where('sentiment IS NULL and created_at > ?', Time.now.utc - 2.hour).each do |mention|
       if mention.comment
         mention.update(sentiment: Sentimentalizer.analyze(mention.comment).overall_probability)
       end
