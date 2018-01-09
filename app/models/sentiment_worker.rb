3797e20f381e1639be4b3fb09622b87e28fda522
John Gregory <john@bellwethermade.com>
2018-01-08 18:49:34 -0800

    adds sentiment_set to mention

--- app/models/sentiment_worker.rb
+++ app/models/sentiment_worker.rb
@@ -6,9 +6,9 @@ class SentimentWorker
   # Sidekiq::Cron::Job.create(name: 'reddit_worker', cron: '7 * * * *', class: 'RedditWorker')
   def perform(*args)
     puts "STARTING SENTIMENT ANALYSIS"
-    Mention.where(source: 'reddit_cryptocurrency', sentiment: nil).where('created_at > ?', Time.now.utc - 10.minutes).each do |mention|
+    Mention.where(sentiment_set: false).where('created_at > ?', Time.now.utc - 10.minutes).each do |mention|
       if mention.comment
-        mention.update(sentiment: Sentimentalizer.analyze(mention.comment).overall_probability)
+        mention.update(sentiment: Sentimentalizer.analyze(mention.comment).overall_probability, sentiment_set: true)
       end
     end
     puts "DONE SENTIMENT ANALYSIS"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180108075405) do
+ActiveRecord::Schema.define(version: 20180109024617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,12 +26,13 @@ ActiveRecord::Schema.define(version: 20180108075405) do
   create_table "mentions", force: :cascade do |t|
     t.integer  "coin_id"
     t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.string   "comment"
     t.string   "post_title"
     t.string   "source"
     t.float    "sentiment"
+    t.boolean  "sentiment_set", default: false
   end
 
 end
